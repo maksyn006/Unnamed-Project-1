acb5178656375fec8e518475fc34c6a75ee4bca0
Maksyn006 <chudnivakk@gmail.com>
2022-06-10 16:42:57 +0300

    add displaymode machine and integreted it

--- bin/main.py
+++ bin/main.py
@@ -2,19 +2,41 @@ from random import randint
 from pygame import *
 
 from bin.settings import *
+from bin.displaymode import *
 init()
 
 sc = display.set_mode((WIDTH, HEIGHT))
 clock = time.Clock()
 
 
-is_run = True
-while is_run:
+def game():
+    if not display_mode['is_game']:
+        return
     for e in event.get():
         if e.type == QUIT:
-            is_run = False
+            set_display_mode_inactive('is_run')
     sc.fill(WHITE)
     draw.rect(sc, RED, (randint(0, WIDTH), randint(0, HEIGHT), 50, 50))
     display.update()
     clock.tick(FPS)
+
+
+def main_menu():
+    if not display_mode['is_main_menu']:
+        return
+    for e in event.get():
+        if e.type == QUIT:
+            set_display_mode_inactive('is_run')
+        if e.type == MOUSEBUTTONDOWN:
+            if 0 < e.pos[0] < 200 and 0 < e.pos[1] < 200:
+                set_display_mode_inactive('is_run')
+    sc.fill(WHITE)
+    draw.rect(sc, GREEN, (randint(0, WIDTH), randint(0, HEIGHT), 50, 50))
+    display.update()
+    clock.tick(FPS)
+
+
+while display_mode['is_run']:
+    game()
+    main_menu()
 quit()
