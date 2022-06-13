5a813ae11c513b94e913a3bb9e600c1e7086bc99
Maksyn006 <chudnivakk@gmail.com>
2022-06-13 13:24:30 +0300

    add mode and skeleton of Game class

--- bin/game.py
+++ bin/game.py
@@ -1,5 +1,18 @@
+from pygame import *
 
 
 class Game:
-    def __init__(self, _path):
+    def __init__(self, session_path: str):
+        self.mode = 'game'
+
+    def update(self):
         pass
+
+    def draw(self, sc):
+        pass
+
+    def handle_events(self):
+        for e in event.get():
+            if e.type == QUIT:
+                self.mode = 'exit'
+
