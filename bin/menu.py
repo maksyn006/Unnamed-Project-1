a10ce36f0dc3f5e24819427b2410476648c569bd
Maksyn006 <chudnivakk@gmail.com>
2022-06-13 13:34:14 +0300

    fixup! add menu sys skeleton

--- bin/menu.py
+++ bin/menu.py
@@ -4,17 +4,4 @@ from bin.button import Button
 
 
 class Menu:
-    def __init__(self):
-        self.buttons = []
-
-    def draw(self, sc):
-        for btn in self.buttons:
-            btn.draw(sc)
-
-    def update(self, e):
-        if e.type == MOUSEBUTTONDOWN:
-            for btn in self.buttons:
-                btn.is_pressed(e.pos)
-
-    def add_button(self, _rect: tuple, text: str, callback):
-        self.buttons.append(Button(_rect, text, callback))
+41
